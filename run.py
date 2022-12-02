675e295b89505c7f2259b6c29232174266977491
zbf <hgkdzbf6@qq.com>
2022-12-02 18:55:23 +0800

    pull功能完善

--- run.py
+++ run.py
@@ -21,7 +21,10 @@ class SyncService():
         print(cmd)
         ret = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
         # ret = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        logs = [item.decode('utf8') for item in ret.stdout.readlines()]
+        try:
+            logs = [item.decode('utf8') for item in ret.stdout.readlines()]
+        except:
+            logs = [item.decode('gb2312') for item in ret.stdout.readlines()]
         # logs2 = [item for item in ret.stderr.readlines()]
         print(logs)
         # print(logs2)
@@ -36,6 +39,14 @@ class SyncService():
 
     def _copy(self, src, dst):
         self._run('rsync -rv %s %s --exclude .git' % (self.cyg_path_rep(src), self.cyg_path_rep(dst)))
+
+    def _remote_exist(self):
+        without_ssh_path = self.remote_path.replace('ssh://', '')
+        remote_ip = without_ssh_path.split('/')[0].split(':')[0]
+        remote_port = without_ssh_path.split('/')[0].split(':')[1]
+        remote_path = without_ssh_path.replace(remote_ip + ':' + remote_port, '')
+        res = self._run('ssh -p %s %s ls %s' % (remote_port, remote_ip, remote_path))
+        return len(res) > 0
 
     def path_rep(self, path):
         if self.sys_str == 'Windows':
@@ -64,18 +75,26 @@ class SyncService():
             print('git path not exist.')
             self._copy(self.content_path, self.git_path)
             self._run('cd %s && git init' % self.git_path)
-            self._run('cd %s && git add . && git commit -m "%s daily upload."' % (self.git_path, self.today))
+            self._run('cd %s && git add . && git commit -m "%s daily upload, upload by %s"' %
+                (self.git_path, self.today, self.sys_str))
             self._run('cd %s && git remote add %s' % (self.git_path, self.remote_path))
         else:
             pass
         return 0
 
     def push(self):
+        # 如果内容不存在，报错退出
         if not os.path.exists(self.content_path):
             print('error, path not exist.')
             return -1
+        # 如果git仓库不存在，初始化仓库
         if not os.path.exists(self.git_path):
-            self.init()
+            if not self._remote_exist():
+                # 新建仓库
+                self.init()
+            else:
+                # 把仓库当中的内容拉下来
+                self.pull()
         # 复制文件
         self._copy(self.content_path, self.git_path)
         # 比较日期
@@ -86,25 +105,30 @@ class SyncService():
         if ny != y or nm != m or nd != d:
             # 需要重新搞一个commit出来
             self._copy(self.content_path, self.git_path)
-            self._run('cd %s && git add . && git commit -m "%s daily upload."' % (self.path_rep(self.git_path), 
-                self.path_rep(self.today)))
+            self._run('cd %s && git add . && git commit -m "%s daily upload, upload by %s"' %
+                (self.path_rep(self.git_path), self.today, self.sys_str))
         else:
             # 需要重新搞一个commit出来
             print('today you have upload another patch')
             self._copy(self.content_path, self.git_path)
-            self._run('cd %s && git add . && git commit -m "%s daily upload."' % (self.path_rep(self.git_path), 
-                self.path_rep(self.today)))
+            self._run('cd %s && git add . && git commit -m "%s daily upload, upload by %s"' %
+                (self.path_rep(self.git_path), self.today, self.sys_str))
             
         self._run('cd %s && git pull origin master' % (self.path_rep(self.git_path)))
         return 0
 
     def pull(self):
+        # 如果仓库不存在，下载仓库
         if not os.path.exists(self.git_path):
             self._run('cd %s && git clone %s' % (self.base_path, self.remote_path))
             self._run('cd %s && git pull origin master' % (self.git_path))
+        # 如果内容不存在，下载内容
+        if not os.path.exists(self.content_path):
+            os.mkdir(self.content_path)
+        # 复制git当中的到目标位置
         self._copy(self.git_path, self.content_path)
 
 if __name__ == '__main__':
     ss = SyncService()
-    ss.push()
     # ss.push()
+    # ss.pull()
